5c34fb4d88aae93a65eb5a55f820086d9b2ed7b9
cornelius <cornelius@65ff189f-35ee-0310-953c-23049635c3cf>
2010-05-08 03:50:48 +0000

    wefollow:   1. remove multisiting   2. update settings for different sites   3. bugfixes

--- src/zojax/django/location/admin.py
+++ src/zojax/django/location/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from django.contrib.sites.models import RequestSite
-from django.contrib.sites.models import Site
-from django.utils.translation import ugettext_lazy as _
-
 from models import City, Country, State
 
 admin.site.register([Country, State, City])
