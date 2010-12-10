b9a9e22a326de39dbbb418dec2c5fddedd993e6c
bubenkoff <bubenkoff@65ff189f-35ee-0310-953c-23049635c3cf>
2010-12-10 02:46:25 +0000

    wrapper added

--- src/zojax/django/forms/templatetags/zojax_form_tags.py
+++ src/zojax/django/forms/templatetags/zojax_form_tags.py
@@ -3,6 +3,9 @@ import copy
 from django import template
 from django import forms
 from django.utils.datastructures import SortedDict
+from django.utils.functional import allow_lazy
+from django.template.defaultfilters import stringfilter
+from django.utils.encoding import force_unicode
 
 register = template.Library()
 
@@ -45,3 +48,59 @@ class FieldSetNode(template.Node):
         context[self.variable_name] = new_form
 
         return u''
+    
+def wrap(text, width):
+    """
+    A word-wrap function that preserves existing line breaks and most spaces in
+    the text. Expects that existing line breaks are posix newlines.
+    """
+    text = force_unicode(text)
+    def _generator():
+        it = iter(text.split(' '))
+        pos=0
+        for word in it:
+            if "\n" in word:
+                lines = word.split('\n')
+            elif len(word) > width:
+                lines = [word[i:i+width] for i in xrange(len(word)/width)]
+                word="\n".join(lines)
+            else:
+                lines = (word,)
+            print lines
+            pos += len(lines[0]) + 1
+            if pos > width:
+                yield '\n'
+                pos = len(lines[-1])
+            else:
+                yield ' '
+                if len(lines) > 1:
+                    pos = len(lines[-1])
+            yield word
+    return u''.join(_generator())
+    
+    
+def smartwordwrap(value, arg):
+    """
+    Wraps words at specified line length.
+
+    Argument: number of characters to wrap the text at.
+    """
+    return wrap(value, int(arg))
+        
+smartwordwrap.is_safe = True
+smartwordwrap = stringfilter(smartwordwrap)
+register.filter(smartwordwrap)
+    
+@register.filter
+def trunc( string, number, dots='...'):
+  """ 
+  truncate the {string} to {number} characters
+  print {dots} on the end if truncated
+
+  usage: {{ "some text to be truncated"|trunc:6 }}
+  results: some te...
+  """
+  if not isinstance(string, str): string = str(string)
+  if len(string) <= number:
+    return string
+  return string[0:number]+dots
